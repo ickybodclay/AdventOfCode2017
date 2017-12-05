40d9adf13dbdb5e6fb7b5ff3383a18967f2eaa10
Jason Petterson <jpetterson@detroitlabs.com>
2017-12-05 18:29:39 -0500

    added part 2 solution for day 5

--- src/Day5.kt
+++ src/Day5.kt
@@ -23,10 +23,30 @@ class Day5 {
             steps++
         }
 
-        println("# of steps = " + steps)
+        println("[Part 1] # of steps = " + steps)
+    }
+
+    fun printPart2Solution() {
+        val instructionArray = readInput()
+        var index = 0;
+        var steps = 0;
+        while (index >= 0 && index < instructionArray.size) {
+            val newIndex = index + instructionArray[index]
+            if (instructionArray[index] >= 3) {
+                instructionArray[index]--
+            }
+            else {
+                instructionArray[index]++
+            }
+            index = newIndex
+            steps++
+        }
+
+        println("[Part 2] # of steps = " + steps)
     }
 }
 
 fun main(args: Array<String>) {
     Day5().printPart1Solution()
+    Day5().printPart2Solution()
 }
